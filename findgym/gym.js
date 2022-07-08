123a3018acdb9ce22e68fcf5486a946d2943afe0
emmanuel parker <emmanuelparker71@gmail.com>
2022-07-08 14:26:38 +0100

    HTML, CSS & JS

--- findgym/gym.js
+++ findgym/gym.js
@@ -14,3 +14,26 @@ navlinkloc.addEventListener("click", () => {
     locations.classList.toggle("active");
 
 })
+
+//section 2 
+var arr = ["image2.jpg", "image3.jpg", "image4.jpg"];
+const section1 = document.getElementById("section1");
+var i = 0;
+
+function Switch() {
+    section1.style.background = `url(${arr[i]})`;
+    section1.style.backgroundSize = "contain";
+    section1.style.backgroundRepeat = "no-repeat";
+    section1.style.width = "100vw";
+    section1.style.height = "500px";
+    i++;
+    console.log(i);
+
+
+    if (i == arr.length){
+        i = 0;
+    } 
+    
+};
+
+setInterval(Switch,4000);
